54461d85dc554fefebd95bbf010cd4d5e6bf344a
ashik <ashik@studevs>
2016-04-02 23:32:08 +0600

    Initial Project Module.

---
+++ src/com/studevs/main/Main.java
@@ -0,0 +1,13 @@
+package com.studevs.main;
+
+/**
+ *
+ * @author ashik
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World.......");
+    }
+}
